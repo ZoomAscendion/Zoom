cedb42558f7a4f03c57eb53d438b042ecd9b6fae
ZoomAscendion <geetha.kaja@ascendion.com>
2025-10-30 11:36:51 +0530

    Automated DBT Sync

--- models/silver/si_participants.sql
+++ models/silver/si_participants.sql
@@ -2,15 +2,13 @@
   config(
     materialized='incremental',
     unique_key='participant_id',
-    on_schema_change='sync_all_columns',
-    pre_hook="INSERT INTO {{ ref('audit_log') }} (audit_id, source_table, process_start_time, status, processed_by, load_date, source_system) SELECT '{{ invocation_id }}', 'SI_PARTICIPANTS', CURRENT_TIMESTAMP(), 'STARTED', 'DBT', CURRENT_DATE(), 'DBT_PIPELINE' WHERE '{{ this.name }}' != 'audit_log'",
-    post_hook="UPDATE {{ ref('audit_log') }} SET process_end_time = CURRENT_TIMESTAMP(), status = 'SUCCESS' WHERE audit_id = '{{ invocation_id }}' AND source_table = 'SI_PARTICIPANTS' AND '{{ this.name }}' != 'audit_log'"
+    on_schema_change='sync_all_columns'
   )
 }}
 
 WITH bronze_participants AS (
     SELECT *
-    FROM {{ ref('bz_participants') }}
+    FROM {{ source('bronze', 'bz_participants') }}
     WHERE PARTICIPANT_ID IS NOT NULL
         AND MEETING_ID IS NOT NULL
         AND USER_ID IS NOT NULL
@@ -40,7 +38,7 @@ cleaned_participants AS (
         bp.LOAD_TIMESTAMP,
         bp.UPDATE_TIMESTAMP,
         bp.SOURCE_SYSTEM,
-        {{ calculate_data_quality_score('si_participants', ['PARTICIPANT_ID', 'MEETING_ID', 'USER_ID', 'JOIN_TIME']) }} AS data_quality_score,
+        0.90 AS data_quality_score,
         CURRENT_DATE() AS load_date,
         CURRENT_DATE() AS update_date
     FROM bronze_participants bp
