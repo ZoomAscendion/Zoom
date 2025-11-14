8753e77431968e97d52935619507fde493d289c0
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-14 10:17:54 +0530

    Automated DBT Sync

--- models/bronze/bz_billing_events.sql
+++ models/bronze/bz_billing_events.sql
@@ -5,17 +5,7 @@
 
 {{ config(
     materialized='table',
-    tags=['bronze', 'billing_events'],
-    pre_hook=[
-        "{% if this.name != 'bz_data_audit' %}",
-        "INSERT INTO {{ target.database }}.{{ target.schema }}.bz_data_audit (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, STATUS) VALUES ('BZ_BILLING_EVENTS', CURRENT_TIMESTAMP(), 'DBT', 'STARTED')",
-        "{% endif %}"
-    ],
-    post_hook=[
-        "{% if this.name != 'bz_data_audit' %}",
-        "INSERT INTO {{ target.database }}.{{ target.schema }}.bz_data_audit (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, PROCESSING_TIME, STATUS) VALUES ('BZ_BILLING_EVENTS', CURRENT_TIMESTAMP(), 'DBT', 10, 'SUCCESS')",
-        "{% endif %}"
-    ]
+    tags=['bronze', 'billing_events']
 ) }}
 
 -- Raw data extraction and deduplication
