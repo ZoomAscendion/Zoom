b007d39430922e7f603712360468454216d727e6
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-17 18:59:21 +0530

    Automated DBT Sync

--- models/silver/si_support_tickets.sql
+++ models/silver/si_support_tickets.sql
@@ -1,7 +1,7 @@
 {{ config(
     materialized='table',
-    pre_hook="INSERT INTO {{ ref('si_audit_log') }} (MODEL_NAME, TABLE_NAME, PROCESS_STATUS, LOAD_TIMESTAMP) SELECT '{{ this.name }}', 'SI_SUPPORT_TICKETS', 'STARTED', CURRENT_TIMESTAMP() WHERE '{{ this.name }}' != 'si_audit_log'",
-    post_hook="INSERT INTO {{ ref('si_audit_log') }} (MODEL_NAME, TABLE_NAME, PROCESS_STATUS, LOAD_TIMESTAMP) SELECT '{{ this.name }}', 'SI_SUPPORT_TICKETS', 'COMPLETED', CURRENT_TIMESTAMP() WHERE '{{ this.name }}' != 'si_audit_log'"
+    pre_hook="INSERT INTO {{ ref('si_audit_log') }} (MODEL_NAME, TABLE_NAME, PROCESS_STATUS, LOAD_TIMESTAMP) SELECT 'si_support_tickets', 'SI_SUPPORT_TICKETS', 'STARTED', CURRENT_TIMESTAMP()",
+    post_hook="INSERT INTO {{ ref('si_audit_log') }} (MODEL_NAME, TABLE_NAME, PROCESS_STATUS, LOAD_TIMESTAMP) SELECT 'si_support_tickets', 'SI_SUPPORT_TICKETS', 'COMPLETED', CURRENT_TIMESTAMP()"
 ) }}
 
 -- Silver Layer Support Tickets Table
