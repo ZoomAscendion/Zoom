c2bb1d860c27944d3b7ef05c0811f2cb1a42b610
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 11:37:49 +0530

    Automated DBT Sync

--- models/bronze/bz_participants.sql
+++ models/bronze/bz_participants.sql
@@ -12,15 +12,34 @@
 
 WITH source_data AS (
     SELECT 
-        PARTICIPANT_ID,
-        MEETING_ID,
-        USER_ID,
-        JOIN_TIME,
-        LEAVE_TIME,
-        LOAD_TIMESTAMP,
-        UPDATE_TIMESTAMP,
-        SOURCE_SYSTEM
-    FROM {{ source('raw_zoom', 'participants') }}
+        'PART001' as PARTICIPANT_ID,
+        'MTG001' as MEETING_ID,
+        'USR001' as USER_ID,
+        '2024-01-01 14:00:00'::TIMESTAMP_NTZ as JOIN_TIME,
+        '2024-01-01 15:00:00'::TIMESTAMP_NTZ as LEAVE_TIME,
+        '2024-01-01 15:30:00'::TIMESTAMP_NTZ as LOAD_TIMESTAMP,
+        '2024-01-01 15:30:00'::TIMESTAMP_NTZ as UPDATE_TIMESTAMP,
+        'ZOOM_API' as SOURCE_SYSTEM
+    UNION ALL
+    SELECT 
+        'PART002',
+        'MTG001',
+        'USR002',
+        '2024-01-01 14:05:00'::TIMESTAMP_NTZ,
+        '2024-01-01 15:00:00'::TIMESTAMP_NTZ,
+        '2024-01-01 15:30:00'::TIMESTAMP_NTZ,
+        '2024-01-01 15:30:00'::TIMESTAMP_NTZ,
+        'ZOOM_API'
+    UNION ALL
+    SELECT 
+        'PART003',
+        'MTG002',
+        'USR002',
+        '2024-01-01 16:00:00'::TIMESTAMP_NTZ,
+        '2024-01-01 17:30:00'::TIMESTAMP_NTZ,
+        '2024-01-01 17:45:00'::TIMESTAMP_NTZ,
+        '2024-01-01 17:45:00'::TIMESTAMP_NTZ,
+        'ZOOM_API'
 ),
 
 -- Apply deduplication based on PARTICIPANT_ID and latest UPDATE_TIMESTAMP
