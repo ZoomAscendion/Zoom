6865a1ba9eee498119df93e6435f82fba9d897ca
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 21:06:06 +0530

    Delete macros/safe_to_number.sql

--- macros/safe_to_number.sql
+++
@@ -1,7 +0,0 @@
-{% macro safe_to_number(column_name, default_value=0) %}
-    CASE 
-        WHEN TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', '')) IS NOT NULL THEN 
-            TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', ''))
-        ELSE {{ default_value }}
-    END
-{% endmacro %}
