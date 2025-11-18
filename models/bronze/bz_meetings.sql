35cb14676c95c4ce68255af92c70ce24ceaca52d
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 11:54:33 +0530

    Automated DBT Sync

--- models/bronze/bz_meetings.sql
+++ models/bronze/bz_meetings.sql
@@ -23,7 +23,7 @@ WITH source_data AS (
         LOAD_TIMESTAMP,
         UPDATE_TIMESTAMP,
         SOURCE_SYSTEM
-    FROM {{ source('raw_schema', 'MEETINGS') }}
+    FROM {{ source('raw', 'MEETINGS') }}
 ),
 
 deduped_data AS (
