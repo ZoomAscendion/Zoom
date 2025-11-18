1972299e254219ee8224e6ae23a36e9f5b40046b
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 13:13:38 +0530

    Automated DBT Sync

--- macros/safe_numeric_conversion.sql
+++ macros/safe_numeric_conversion.sql
@@ -1,7 +1,7 @@
 {% macro safe_numeric_conversion(column_name) %}
-    CASE 
-        WHEN TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', '')) IS NOT NULL THEN
-            TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', ''))
-        ELSE TRY_TO_NUMBER({{ column_name }}::STRING)
-    END
+  CASE 
+    WHEN TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', '')) IS NOT NULL THEN
+         TRY_TO_NUMBER(REGEXP_REPLACE({{ column_name }}::STRING, '[^0-9.]', ''))
+    ELSE TRY_TO_NUMBER({{ column_name }}::STRING)
+  END
 {% endmacro %}
