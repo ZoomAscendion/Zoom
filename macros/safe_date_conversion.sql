ca0f581a9cda9cdf1425528b58e2f9154a0808b9
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 13:13:39 +0530

    Automated DBT Sync

--- macros/safe_date_conversion.sql
+++ macros/safe_date_conversion.sql
@@ -1,8 +1,8 @@
 {% macro safe_date_conversion(column_name) %}
-    COALESCE(
-        TRY_TO_DATE({{ column_name }}::STRING, 'YYYY-MM-DD'),
-        TRY_TO_DATE({{ column_name }}::STRING, 'DD/MM/YYYY'),
-        TRY_TO_DATE({{ column_name }}::STRING, 'MM/DD/YYYY'),
-        TRY_TO_DATE({{ column_name }}::STRING)
-    )
+  COALESCE(
+    TRY_TO_DATE({{ column_name }}::STRING, 'YYYY-MM-DD'),
+    TRY_TO_DATE({{ column_name }}::STRING, 'DD/MM/YYYY'),
+    TRY_TO_DATE({{ column_name }}::STRING, 'MM/DD/YYYY'),
+    TRY_TO_DATE({{ column_name }}::STRING)
+  )
 {% endmacro %}
