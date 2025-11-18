9a36ee48197dfee05d29941ac0e7bd48ca407e24
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 13:13:40 +0530

    Automated DBT Sync

--- macros/safe_timestamp_conversion.sql
+++ macros/safe_timestamp_conversion.sql
@@ -1,8 +1,13 @@
 {% macro safe_timestamp_conversion(column_name) %}
-    COALESCE(
+  CASE 
+    WHEN {{ column_name }}::STRING LIKE '%EST%' THEN 
+      TRY_TO_TIMESTAMP(REGEXP_REPLACE({{ column_name }}::STRING, '\\s*(EST|PST|CST|IST|UTC)', ''), 'YYYY-MM-DD HH24:MI:SS')
+    ELSE 
+      COALESCE(
         TRY_TO_TIMESTAMP({{ column_name }}::STRING, 'YYYY-MM-DD HH24:MI:SS'),
         TRY_TO_TIMESTAMP({{ column_name }}::STRING, 'DD/MM/YYYY HH24:MI'),
         TRY_TO_TIMESTAMP({{ column_name }}::STRING, 'MM/DD/YYYY HH24:MI'),
         TRY_TO_TIMESTAMP({{ column_name }}::STRING)
-    )
+      )
+  END
 {% endmacro %}
