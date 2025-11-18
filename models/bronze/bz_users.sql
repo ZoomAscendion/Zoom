684ad999a9f3f6f1945df3b028f32929d296f1af
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 18:41:28 +0530

    Automated DBT Sync

--- models/bronze/bz_users.sql
+++ models/bronze/bz_users.sql
@@ -5,9 +5,7 @@
 
 {{ config(
     materialized='table',
-    tags=['bronze', 'users'],
-    pre_hook="INSERT INTO {{ ref('bz_data_audit') }} (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, STATUS) SELECT 'BZ_USERS', CURRENT_TIMESTAMP(), 'DBT_BZ_USERS', 'STARTED' WHERE '{{ this.name }}' != 'bz_data_audit'",
-    post_hook="INSERT INTO {{ ref('bz_data_audit') }} (SOURCE_TABLE, LOAD_TIMESTAMP, PROCESSED_BY, PROCESSING_TIME, STATUS) SELECT 'BZ_USERS', CURRENT_TIMESTAMP(), 'DBT_BZ_USERS', 1.0, 'COMPLETED' WHERE '{{ this.name }}' != 'bz_data_audit'"
+    tags=['bronze', 'users']
 ) }}
 
 WITH source_data AS (
