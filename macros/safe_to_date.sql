b2c151dc1ed6d426427e40bf19cd966b4d89b670
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 10:50:17 +0530

    Delete macros/safe_to_date.sql

--- macros/safe_to_date.sql
+++
@@ -1,8 +0,0 @@
-{% macro safe_to_date(column_name) %}
-    COALESCE(
-        TRY_TO_DATE({{ column_name }}::STRING, 'YYYY-MM-DD'),
-        TRY_TO_DATE({{ column_name }}::STRING, 'DD/MM/YYYY'),
-        TRY_TO_DATE({{ column_name }}::STRING, 'MM/DD/YYYY'),
-        {{ column_name }}
-    )
-{% endmacro %}
