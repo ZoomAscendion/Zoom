5bcc2bfa90d4db5c98c47b1571620ebb3b81a084
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-18 22:55:32 +0530

    Automated DBT Sync

--- models/silver/SI_Billing_Events.sql
+++ models/silver/SI_Billing_Events.sql
@@ -1,7 +1,5 @@
 {{ config(
-    materialized='table',
-    pre_hook="INSERT INTO {{ ref('SI_Audit_Log') }} (AUDIT_ID, TABLE_NAME, OPERATION_TYPE, AUDIT_TIMESTAMP, PROCESSED_BY) SELECT UUID_STRING(), 'SI_BILLING_EVENTS', 'PIPELINE_START', CURRENT_TIMESTAMP(), 'DBT_SILVER_PIPELINE' WHERE '{{ this.name }}' != 'SI_Audit_Log'",
-    post_hook="INSERT INTO {{ ref('SI_Audit_Log') }} (AUDIT_ID, TABLE_NAME, OPERATION_TYPE, AUDIT_TIMESTAMP, PROCESSED_BY) SELECT UUID_STRING(), 'SI_BILLING_EVENTS', 'PIPELINE_END', CURRENT_TIMESTAMP(), 'DBT_SILVER_PIPELINE' WHERE '{{ this.name }}' != 'SI_Audit_Log'"
+    materialized='table'
 ) }}
 
 -- SI_Billing_Events table transformation from Bronze to Silver
@@ -17,7 +15,7 @@ WITH bronze_billing_events AS (
         LOAD_TIMESTAMP,
         UPDATE_TIMESTAMP,
         SOURCE_SYSTEM
-    FROM {{ source('bronze', 'BZ_BILLING_EVENTS') }}
+    FROM BRONZE.BZ_BILLING_EVENTS
 ),
 
 cleaned_billing_events AS (
@@ -43,7 +41,7 @@ validated_billing_events AS (
         *,
         DATE(LOAD_TIMESTAMP) AS LOAD_DATE,
         DATE(UPDATE_TIMESTAMP) AS UPDATE_DATE,
-        -- Calculate data quality score
+        /* Calculate data quality score */
         CASE 
             WHEN EVENT_ID IS NOT NULL 
                 AND USER_ID IS NOT NULL 
@@ -57,7 +55,7 @@ validated_billing_events AS (
             THEN 75
             ELSE 50
         END AS DATA_QUALITY_SCORE,
-        -- Set validation status
+        /* Set validation status */
         CASE 
             WHEN EVENT_ID IS NOT NULL 
                 AND USER_ID IS NOT NULL 
