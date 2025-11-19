a102d89de1ccbc4d0ebe25237b6c3f89653a74e3
ZoomAscendion <geetha.kaja@ascendion.com>
2025-11-19 17:41:47 +0530

    Refactor pre_hook to use UUID_STRING for audit log

--- models/gold/dimension/go_dim_meeting_type.sql
+++ models/gold/dimension/go_dim_meeting_type.sql
@@ -1,7 +1,36 @@
 {{ config(
     materialized='table',
     tags=['dimension', 'gold'],
-    pre_hook="INSERT INTO {{ ref('go_audit_log') }} SELECT {{ dbt_utils.generate_surrogate_key(['GO_DIM_MEETING_TYPE', 'CURRENT_TIMESTAMP()']) }} AS audit_log_id, 'GO_DIM_MEETING_TYPE' AS process_name, 'DIMENSION_LOAD' AS process_type, CURRENT_TIMESTAMP() AS execution_start_timestamp, NULL AS execution_end_timestamp, NULL AS execution_duration_seconds, 'RUNNING' AS execution_status, 'SI_MEETINGS' AS source_table_name, 'GO_DIM_MEETING_TYPE' AS target_table_name, 0 AS records_read, 0 AS records_processed, 0 AS records_inserted, 0 AS records_updated, 0 AS records_failed, 100.0 AS data_quality_score, 0 AS error_count, 0 AS warning_count, 'DBT_RUN' AS process_trigger, 'DBT_SYSTEM' AS executed_by, 'DBT_SERVER' AS server_name, '1.0.0' AS process_version, PARSE_JSON('{}') AS configuration_parameters, PARSE_JSON('{}') AS performance_metrics, CURRENT_DATE() AS load_date, CURRENT_DATE() AS update_date, 'DBT_GOLD_PIPELINE' AS source_system",
+    pre_hook="INSERT INTO {{ ref('go_audit_log') }}
+    SELECT 
+    UUID_STRING() AS audit_log_id,
+    'GO_DIM_MEETING_TYPE' AS process_name,
+    'DIMENSION_LOAD' AS process_type,
+    CURRENT_TIMESTAMP() AS execution_start_timestamp,
+    NULL AS execution_end_timestamp,
+    NULL AS execution_duration_seconds,
+    'RUNNING' AS execution_status,
+    'SI_MEETINGS' AS source_table_name,
+    'GO_DIM_MEETING_TYPE' AS target_table_name,
+    0 AS records_read,
+    0 AS records_processed,
+    0 AS records_inserted,
+    0 AS records_updated,
+    0 AS records_failed,
+    100.0 AS data_quality_score,
+    0 AS error_count,
+    0 AS warning_count,
+    'DBT_RUN' AS process_trigger,
+    'DBT_SYSTEM' AS executed_by,
+    'DBT_SERVER' AS server_name,
+    '1.0.0' AS process_version,
+    PARSE_JSON('{}') AS configuration_parameters,
+    PARSE_JSON('{}') AS performance_metrics,
+    CURRENT_DATE() AS load_date,
+    CURRENT_DATE() AS update_date,
+    'DBT_GOLD_PIPELINE' AS source_system
+"
+
     post_hook="UPDATE {{ ref('go_audit_log') }} SET execution_end_timestamp = CURRENT_TIMESTAMP(), execution_status = 'SUCCESS', records_processed = (SELECT COUNT(*) FROM {{ this }}), execution_duration_seconds = DATEDIFF('second', execution_start_timestamp, CURRENT_TIMESTAMP()) WHERE process_name = 'GO_DIM_MEETING_TYPE' AND execution_status = 'RUNNING'"
 ) }}
 
