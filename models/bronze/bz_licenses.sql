603a94d0ccdf7c050c26051b97a79468a21c6ee1
DIAscendion <geetha.kaja@ascendion.com>
2025-12-01 15:57:04 +0530

    Automated DBT Sync

--- models/bronze/bz_licenses.sql
+++ models/bronze/bz_licenses.sql
@@ -21,7 +21,7 @@ WITH source_data AS (
         LOAD_TIMESTAMP,
         UPDATE_TIMESTAMP,
         SOURCE_SYSTEM
-    FROM {{ source('raw_schema', 'licenses') }}
+    FROM {{ source('raw', 'licenses') }}
     WHERE LICENSE_ID IS NOT NULL  -- Filter out null primary keys
 ),
 
